01fae91367a08ee80b1e314974e51afdc38a870f
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-06-25 11:30:49 +0300

    Action Creators, Redux and Thunk

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Things learnt during current project:
 
+- redux concepts and the idea of state management
+- redux-thunk to add async functionality to redux which is sync in its nature
+
 Things made in different way:
 
 - project generated with `yarn create @vitejs/app`
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
-import { createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const reduxDebug = import.meta.env.VITE_REDUX_DEBUG
 
-const store = createStore(
-  rootReducer,
-  !!reduxDebug && (window as any).__REDUX_DEVTOOLS_EXTENSION__?.()
-)
+const composeEnhancer =
+  (!!reduxDebug && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
 
 export default store
