260d77caadfae6fc25e45d5079f330c856079bc0
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-06-26 15:57:28 +0300

    Update to use local proxy

--- src/util/API.ts
+++ src/util/API.ts
@@ -1,26 +1,9 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://api.rawg.io/api'
-const POPULAR_GAMES_PATH = `/games?dates=${yyyyMMdd(
-  -1
-)},${yyyyMMdd()}&ordering=-rating&page_size=10`
-
-const http = axios.create({ baseURL: BASE_URL })
-
-function yyyyMMdd(yearStep: -1 | 0 = 0) {
-  const _ = (part: number) => (part < 10 ? `0${part}` : part)
-
-  const date = new Date()
-
-  const year = date.getFullYear() + yearStep
-  const month = _(date.getMonth())
-  const day = _(date.getDate())
-
-  return `${year}-${month}-${day}`
-}
+const http = axios.create({})
 
 function fetchPopularGames() {
-  return http.get(POPULAR_GAMES_PATH)
+  return http.get('/api/popular')
 }
 
 export { fetchPopularGames }
